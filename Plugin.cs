154f81c5d812cbc7584989f5ea3c3d41b568cf83
Michael Nelsen <mike@hedgren.org>
2022-06-25 21:05:47 -0700

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using System.IO;
+
+namespace BuildWithFrostner {
+	[BepInPlugin("nrg.valheim.buildwithfrostner", "BuildWithFrostner", "1.0.0")]
+	public class BuildWithFrostner : BaseUnityPlugin {
+		private ConfigEntry<string> cfgKey;
+
+		private void Awake() {
+			cfgKey = Config.Bind(
+				"General", // config section
+				"Toggle Key", // config key
+				"Alt", // default value
+				"Control which key will toggle Frostner between attack and build modes" // config description
+			);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Build with Frostner
+
+Allows you to use Frostner as a build hammer
+
+
